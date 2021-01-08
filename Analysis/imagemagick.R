f1f14d4f5a91b6bd36c05a0e8d30bd72b9540652
Dan <danielfaltesek@gmail.com>
2021-01-08 15:58:56 -0800

    new functions

---
+++ Analysis/imagemagick.R
@@ -0,0 +1,15 @@
+#analysis test from image R
+
+#takes result from images
+
+measure_images<-function(x){
+  library(magick)
+  nerb<-image_read(images$local_path)
+  text<-cat(image_ocr(nerb))
+  meta<-image_info(nerb)
+  print(text)
+  print(meta)
+  
+}
+
+measure_images(images)
---
+++ Import/loader.R
@@ -0,0 +1,21 @@
+#calls the working directory
+
+#perhaps add another function to name the output dataframe
+load_images<-function(y){
+  #x in this case is the name of the directory with the images
+  #images would be a great example
+  working<-getwd()
+  #return file list, full.names allows passage of the entire file paths
+  return1<-list.files(y, full.names = TRUE)
+  #prints proof of concept
+  print(return1)
+  #full paths, if needed
+  return2<-paste(working,return1, sep="")
+  #assign back to global environment
+  images<<-data.frame("local_path"=return1, "global_path"=return2)
+}
+
+#test code
+load_images("Images")
+
+
