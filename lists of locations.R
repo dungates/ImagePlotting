c94e269d45da3b7a969ce60b4f973c3b381c67f0
Dan <danielfaltesek@gmail.com>
2021-02-25 00:01:22 -0800

    List of locations for subfiles

---
+++ lists of locations.R
@@ -0,0 +1,26 @@
+#matrix reduces
+
+#upper left
+c(R1,R2,R3,R4,
+  R9,R10,R11,R12,
+  R17,R18,R19,R20, 
+  R25,R26,R27,R28)
+
+#upper right 
+c(R5,R6,R7,R8,
+  R13,R14,R15,R16,
+  R21,R22,R23,R24,
+  R29,R30,R31,R32)
+
+#lower left
+c(R33,R34,R35,R36,
+  R41,R42,R43,R44,
+  R49,R50,R51,R52,
+  R57,R58,R59,R60)
+
+#lower right
+c(R37,R38,R39,R40,
+  R45,R46,R47,R48,
+  R53,R54,R55,R56,
+  R61,R62,R63,R64)
+
