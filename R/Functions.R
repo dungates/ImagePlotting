982beb4ed01ebd5acc8c906fe34d6bed45acdb6e
Dan <danielfaltesek@gmail.com>
2021-07-07 13:16:04 -0700

    Updated Color Analysis

--- R/Functions.R
+++ R/Functions.R
@@ -680,7 +680,7 @@ symmetry_lower <- function(x) {
 }
 
 lower_colors <- function(x) {
-  loader <- loadImage(x, sample.size = 5000)
+  loader <- colordistance::loadImage(x, sample.size = 5000)
   plot <- loader$filtered.rgb.2d
   plot2 <- data.frame(plot)
   mean_red <- mean(plot2$r * 255)
@@ -701,49 +701,15 @@ lower_colors <- function(x) {
   mean_value <- mean(plot4$v)
   deviation_value <- sd(plot4$v)
   
-  # six hue algor - processes from the colors in process
-  # functions pass by FALSE only report on true
-  if (mean_blue > mean_red) {
-    if (mean_red >= mean_green) {
-      hue_region <- "Violet"
-    }
-    else {
-      if (mean_blue < mean_green) {
-        hue_region <- "Spring Green"
-      }
-    }
-  }
-  if (mean_green > mean_red) {
-    if (mean_red >= mean_blue) {
-      hue_region <- "Chartreuse"
-    }
-    else {
-      if (mean_green < mean_blue) {
-        hue_region <- "Azure"
-      }
-    }
-  }
-  if (mean_red >= mean_green) {
-    if (mean_green >= mean_blue) {
-      hue_region <- "Orange"
-    }
-    else {
-      if (mean_red < mean_blue) {
-        hue_region <- "Violet"
-      }
-    }
-  }
-  
   # luminance
   luminance <- (mean_red + mean_blue + mean_green) / 3
   # brightness with deviation of brightness
   lum_contrast <- (deviation_red + deviation_blue + deviation_green) / 3
-  
+
   
   # push to global environment
   colors_results <<- data.frame(
     mean_red, deviation_red, mean_blue, deviation_blue, mean_green, deviation_green,
-    mean_hue, deviation_hue, mean_saturation, deviation_saturation, mean_value,
-    hue_region, deviation_saturation, luminance, lum_contrast
+    mean_hue, deviation_hue, mean_saturation, deviation_saturation, mean_value, deviation_saturation, luminance, lum_contrast
   )
 }
